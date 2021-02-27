8eb2d0b154e1171a49b6a745da9fc22466418f82
adimerpaul <adimer101@gmail.com>
2021-02-26 20:41:13 -0400

    conexion modificada para la hora

--- Conexion.php
+++ Conexion.php
@@ -1,7 +1,8 @@
 <?php
 function conectar()
 {
-$conex= mysqli_connect("localhost","example_user","password")or die ("Falla de conexion");
-mysqli_select_db($conex,"example_database");
+    date_default_timezone_set('America/La_Paz');
+    $conex= mysqli_connect("localhost","example_user","password")or die ("Falla de conexion");
+    mysqli_select_db($conex,"example_database");
 return $conex;
 };
