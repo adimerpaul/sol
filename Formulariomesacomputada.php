4bdea1253763bfef420ba8bc968889284e1196fe
Naldo2594 <capri.2594@gmail.com>
2021-03-05 19:04:28 -0700

    mesascomputadas

---
+++ Formulariomesacomputada.php
@@ -0,0 +1,112 @@
+<?php
+session_start();
+if($_SESSION['usr']!="")
+{
+ include ("header.php");
+$idmunicipio=$_GET['idmunicipio'];
+$idprovincia=$_GET['idprovincia'];
+ ?>
+  <!-- end header -->
+  <head>
+    <script>
+      function buscar() {
+          var textoBusqueda = $("input#busqueda").val();
+          // console.log(textoBusqueda);
+          // return false;
+          if (textoBusqueda != '') {
+              $.post("busrecintoparamesa.php", {valorBusqueda: textoBusqueda}, function(mensaje) {
+                  if (mensaje!=''){
+                      $("#resultadoBusqueda").html(mensaje);
+                  }
+              }); 
+          } else { 
+              $("#resultadoBusqueda").html('<p>No hay nada que mostrar</p>');
+          };
+      };
+    </script>
+
+  </head>
+  <section id="inner-headline">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <ul class="breadcrumb">
+          <li><a href="index2.php"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
+          <li><a href="#">Registro</a><i class="icon-angle-right"></i></li>
+          <li class="active">Nueva Votación</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  </section>
+  <section id="content">
+    <div class="container">
+        <h1>Provincia : <?php echo($nombreprovincia) ?> - Municipio : <?php echo($nombremunicipio) ?></h1>
+        <?php  
+        if ($_SESSION['usr']=='FJR065') {
+        ?>
+        <center>
+        <h3>GRÁFICOS TOTALES ESTADÍSTICOS DE ESTE MUNICIPIO</h3>
+        <a <?php echo "href='grafics/GraficoAlcaldeMunicipio.php?idmunicipio=$idmunicipio'";?> class="btn btn-large btn-info"> <i class="fa fa-signal"></i> Ver Gráfico de Alcalde</a>
+        <a <?php echo "href='grafics/GraficoConcejalMunicipio.php?idmunicipio=$idmunicipio'";?> class="btn btn-large btn-info"> <i class="fa fa-signal"></i> Ver Gráfico de Concejal</a>
+        <a <?php echo "href='grafics/GraficoAsambleMunicipio.php?idmunicipio=$idmunicipio'";?> class="btn btn-large btn-info"> <i class="fa fa-signal"></i> Ver Gráfico de A. Territorio</a>
+        <a <?php echo "href='grafics/GraficoAsamblePMunicipio.php?idmunicipio=$idmunicipio'";?> class="btn btn-large btn-info"> <i class="fa fa-signal"></i> Ver Gráfico de A. Población</a>
+        <a <?php echo "href='grafics/GraficoGoberMunicipio.php?idmunicipio=$idmunicipio'";?> class="btn btn-large btn-info"> <i class="fa fa-signal"></i> Ver Gráfico de Gobernador</a>
+        </center><br>
+        <?php 
+        }
+        ?>
+    </div>
+    <div class="container">
+      <form>
+        <h2><label>Por favor realíce la búsqueda</label> <small>del recinto electoral.</small></h2>
+        <?php
+        if ($_SESSION['usr']=='FJR065') {
+        ?>
+        <div class="row">
+              <center>
+              <h3>Escriba el nombre de su recinto <i class="fa fa-angle-down"></i></h3> <input type="text" name="busqueda" id="busqueda" value="" placeholder="" maxlength="50" class="form-control " style="text-transform:uppercase;" autocomplete="off" onKeyUp="buscar();"/>
+              </center>
+        </div>
+        <?php
+        }
+        ?>
+        <hr class="colorgraph">
+        <div class="row" id="resultadoBusqueda">
+          <?php
+          // Rescatamos el parámetro que nos llega mediante la url que invoca xmlhttp
+          $municipio=$_GET["idmunicipio"];
+          $provincia=$_GET["idprovincia"];
+                $result=mysqli_query($cnx,"SELECT r.idrecinto,r.recinto,r.cantidadmesa
+                          FROM municipio m INNER JOIN recinto r ON m.idmunicipio=r.idmunicipio 
+                            WHERE r.idmunicipio=3");
+          while ($fila = mysqli_fetch_array($result)){
+          echo "<a href='Listamesascomputadas.php?idrecinto=$fila[0]'><div class='col-lg-4'>
+            <div class='pricing-box-alt special'>
+              <div class='pricing-heading'>
+                <h4><strong>$fila[1]</strong></h4>
+              </div>
+              <div class='pricing-terms'>
+              <h5>$fila[2] Mesas Registradas</h5>
+              </div></a>
+            </div>
+          </div>";
+          }
+          ?> 
+        </div>
+        <hr class="colorgraph">
+      </form>
+    </div>
+  </section>
+
+</div>
+<?php }
+else
+{
+?>
+<script type="text/javascript">alert("Primero debes acceder con tu cuenta filial");</script>
+<?php
+    echo '<META HTTP-EQUIV="Refresh" CONTENT ="0; URL=index.php">';
+};
+include ("footer.php")
+?>
---
+++ Listamesascomputadas.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+if($_SESSION['usr']!="")
+{
+ include ("header.php");
+?>
+<style type="text/css">
+    #th{
+        color:steelblue;
+        font-size:120%;
+    }
+    #th2{
+        color:#656565;
+        font-size:150%;
+    }
+</style>
+	<!-- end header -->
+	<section id="inner-headline">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<ul class="breadcrumb">
+					<li><a href="index2.php"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
+					<li><a href="#">Sistema</a><i class="icon-angle-right"></i></li>
+					<li class="active">Vista de Mesas y sus estados</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	</section>
+    <?php
+            $idrecinto=$_GET['idrecinto'];
+            $recinto=mysqli_query($cnx,"SELECT * FROM recinto WHERE idrecinto=$idrecinto");
+            $frecinto=mysqli_fetch_array($recinto);
+    ?>
+	<section id="content">
+			<div class="row">
+			    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+                    <h1>Recinto : <?php echo($frecinto[4]) ?></h1>
+                    <center><h3>Orden de llenado de votación <big>--></big></h3></center>
+					<table class="table responsive-utilities table-bordered table-hover">
+                        <tr>
+                            <th id='th2'>NÚMERO DE MESA</th><th id='th2'>VOTACION 1</th><th id='th2'>VOTACION 2</th><th id='th2'>VOTACION 3</th><th id='th2'> VOTACION 4</th><th id='th2'>VOTACION 5</th>
+                        </tr>
+                        <?php
+                        $n=0;
+                        echo "<tr>";
+                        $result=mysqli_query($cnx,"SELECT * FROM mesa WHERE idrecinto=$idrecinto");
+                        while ($row=mysqli_fetch_array($result)) {
+                            $n=$n+1;
+                            echo "<td id='th'>Mesa: $n</td>";
+                            $mesavotada=mysqli_query($cnx,"SELECT *
+                            FROM tipocandidatura t
+                            WHERE EXISTS (
+                            SELECT *
+                            FROM votacion v
+                            WHERE t.idtipocandidatura = v.idtipocandidatura AND idmesa=$row[0]
+                            )");
+                            while ($fmesa=mysqli_fetch_array($mesavotada)) {
+                            echo "<td id='th'><a href=>$fmesa[1]</a></td>";
+                            };
+                        echo"</tr>";
+                        };
+                        ?>
+                    </table>
+				</div>
+			</div>
+	</section>
+</div>
+<?php }
+else
+{
+?>
+<script type="text/javascript">alert("Primero debes acceder con tu cuenta filial");</script>
+<?php
+    echo '<META HTTP-EQUIV="Refresh" CONTENT ="0; URL=Login.php">';
+};
+include ("footer.php")
+?>
