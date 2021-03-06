64af9080465b526955bc5e46a9a2c2965c2e6417
Naldo2594 <capri.2594@gmail.com>
2021-03-06 07:49:01 -0700

    Update Conexion.php

--- Conexion.php
+++ Conexion.php
@@ -2,7 +2,7 @@
 function conectar()
 {
     date_default_timezone_set('America/La_Paz');
-    $conex= mysqli_connect("localhost","root","sol123Sol")or die ("Falla de conexion");
+    $conex= mysqli_connect("localhost","example_user","password")or die ("Falla de conexion");
     mysqli_select_db($conex,"example_database");
 return $conex;
 };
