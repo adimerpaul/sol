f8498014cb44ea359e9a26704e48417ec59b3614
Naldo2594 <capri.2594@gmail.com>
2021-03-07 07:23:32 -0700

    info para todos

--- Formulariorecintomesa.php
+++ Formulariorecintomesa.php
@@ -87,21 +87,9 @@ $idprovincia=$_GET['idprovincia'];
           $municipio=$_GET["idmunicipio"];
           $provincia=$_GET["idprovincia"];
           if ($municipio) {
-            if ('FJR065'=='FJR065')
-            {
                 $result=mysqli_query($cnx,"SELECT r.idrecinto,r.recinto,r.cantidadmesa
                           FROM municipio m INNER JOIN recinto r ON m.idmunicipio=r.idmunicipio 
                             WHERE r.idmunicipio=$municipio");
-            }
-            else
-            {
-            $result=mysqli_query($cnx,"SELECT r.idrecinto,r.recinto,r.cantidadmesa
-                          FROM municipio m INNER JOIN recinto r ON m.idmunicipio=r.idmunicipio 
-                            INNER JOIN filial f                            
-                            ON r.idrecinto=f.idrecinto
-                            WHERE r.idmunicipio=$municipio
-                            AND f.usuario='".$_SESSION['usr']."'");
-            }
           while ($fila = mysqli_fetch_array($result)){
           echo "<a href='Formulariomesa.php?idrecinto=$fila[0]'><div class='col-lg-4'>
             <div class='pricing-box-alt special'>
