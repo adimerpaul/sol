b6b39332f0598f5318c2fca0c520ce2f9814befd
Naldo2594 <capri.2594@gmail.com>
2021-03-10 02:18:19 -0700

    Update GraficoAlcaldeMunicipio.php

--- grafics/GraficoAlcaldeMunicipio.php
+++ grafics/GraficoAlcaldeMunicipio.php
@@ -346,7 +346,7 @@ $porcentaje=($cantidadvotada/$cantidadtotal)*100
         $porcentaje=$suma*100;
         $porciento=$porcentaje/223133;
         ?>
-       <br> <i class="glyphicon glyphicon-remove"></i> <big>PORCENTAJE COMPUTADO: <strong class="votosnovalidos"><?=$porciento?> VOTOS</strong></big>
+       <br> <i class="glyphicon glyphicon-remove"></i> <big>PORCENTAJE COMPUTADO: <strong class="votosnovalidos"><?=$porciento?>%</strong></big>
         </center>
     </figure>
     <figure class="highcharts-figure">
